7d813cd3925a3fbf016d244ed767ca7950977280
Aleksandr Matveev <matveev.smr@gmail.com>
2021-02-14 07:32:56 +0300

    homework 6 terraform-1

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "C:/Users/matveev.al/.ssh/ubuntu.pub"
+}
+variable image_id {
+  description = "Disk image"
+}
+variable subnet_id {
+  description = "Subnet"
+}
+variable service_account_key_file {
+  description = "D:/matveev/otus/devops/terraform/key.json"
+}
+variable private_key_path {
+  description = "C:/Users/matveev.al/.ssh/ubuntu"
+}
